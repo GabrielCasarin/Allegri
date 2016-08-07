fbdde6d4e77565890db07c3e07fe4f07029a2e4e
Gabriel Casarin da Silva <gabriel.casarin@hotmail.com>
2016-08-06 21:42:19 -0300

    Criada a classe Simulador

---
+++ Allegri/comum/simulador.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2016 Gabriel Casarin da Silva, All Rights Reserved.
+
+
+from lista_encadeada import ListaEncadeada
+
+
+class Simulador:
+    def __init__(self):
+        super(Simulador, self).__init__()
+        self.__listaEventos = ListaEncadeada()
+
+    def trata_evento(self, evento):
+        pass
+
+    def run(self):
+        while self.__listaEventos:
+            proximo_evento = self.__listaEventos.remove()
+            self.trata_evento(proximo_evento)
